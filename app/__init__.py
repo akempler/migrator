cf4e9db781564a3560d53fc3b77613612c0af181
Adam Kempler <akempler@gmail.com>
2024-12-25 00:29:29 -0500

    Move home page to new package directory.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from config import Config
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
-    
-    from app import routes
-    app.register_blueprint(routes.bp)
-    
+    app.config.from_object(config_class)
+
+    # Register blueprints
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    # from app.migration import bp as migration_bp
+    # app.register_blueprint(migration_bp, url_prefix='/migration')
+
     return app 
---
+++ app/main/routes.py
@@ -0,0 +1,10 @@
+from flask import render_template
+from app.main import bp
+
+@bp.route('/')
+def index():
+    return render_template('main/home.html')
+
+@bp.route('/about')
+def about():
+    return render_template('main/about.html') 
