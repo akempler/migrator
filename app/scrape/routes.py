adf54d11a66dbc4e1a9fcafb513af001753ed8ee
Adam Kempler <akempler@gmail.com>
2025-01-01 18:02:54 -0500

    Move scrape form to separate template. Update dashboard with placeholder.

--- app/scrape/routes.py
+++ app/scrape/routes.py
@@ -1,11 +1,11 @@
 from flask import render_template
 from app.scrape import bp
 
-@bp.route('/scrape')
-def scrape():
-    return render_template('scrape/scrape.html')
-
 @bp.route('/scrape_form', methods=['GET'])
 def scrape_form():
+    return render_template('scrape/scrape_form.html')
+
+@bp.route('/scrape', methods=['POST'])
+def scrape():
     url = request.args.get('url', '')  # Get URL from query parameters
     return render_template('scrape/scrape.html', url=url)
